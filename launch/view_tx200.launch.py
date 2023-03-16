126052de6092898e89a32687f8a704b9bbc5b040
lvquang0507 <levinhquang757@gmail.com>
2023-03-16 17:32:00 +0700

    clean up launch file

--- launch/view_tx200.launch.py
+++ launch/view_tx200.launch.py
@@ -1,52 +1,15 @@
-# Copyright (c) 2022, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-#
-# Author: Dr. Denis
-#
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    PathJoinSubstitution,
+)
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    # Declare arguments
-    declared_arguments = []
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "description_package",
-            default_value="robot-arm",
-            description="Description package of the tx200. Usually the argument is not set, \
-        it enables use of a custom description.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "prefix",
-            default_value='""',
-            description="Prefix of the joint names, useful for \
-        multi-robot setup. If changed than also joint names in the controllers' configuration \
-        have to be updated.",
-        )
-    )
-
-    # Initialize Arguments
-    description_package = LaunchConfiguration("description_package")
-    prefix = LaunchConfiguration("prefix")
+    description_package = "robot-arm"
 
     # Get URDF via xacro
     robot_description_content = Command(
@@ -56,10 +19,6 @@ def generate_launch_description():
             PathJoinSubstitution(
                 [FindPackageShare(description_package), "urdf", "tx200.urdf.xacro"]
             ),
-            " ",
-            "prefix:=",
-            prefix,
-            " ",
         ]
     )
 
@@ -88,8 +47,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription(
-        declared_arguments
-        + [
+        [
             joint_state_publisher_node,
             robot_state_publisher_node,
             rviz_node,
